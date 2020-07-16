63377f9fe5b6aee509e6055edf9975b4f39ca50c
Lucas Santos <santos@santos-pc.localdomain>
2020-07-15 22:08:23 -0300

    refactor(scm): loading .env variables
    Refactored way to use environment variables

--- scm/api/github.js
+++ scm/api/github.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const keys = require("../config/keys");
-
 // @route   GET api/profile/test
 // @desc    Test Posts Route
 // @access  Public
@@ -17,18 +15,17 @@ router.get("/:username/:count/:sort", (req, res) => {
   const username = req.params.username;
   const count = req.params.count;
   const sort = req.params.sort;
-  const clientId = keys.clientId.trim();
-  const clientSecret = keys.clientSecret.trim();
-  const url = `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${
-    clientId
-  }&client_secret=${clientSecret}`;
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+  const url = `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`;
 
   axios
     .get(url)
-    .then(response => {
+    .then((response) => {
       res.json(response.data);
     })
-    .catch(err => {
+    .catch((err) => {
       res
         .status(err.response.status)
         .json({ message: err.response.statusText });
